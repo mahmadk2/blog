293e2f515f302186a3609c7a46b29ac7df16a33a
ahmad <mahmadk2@hotmail.com>
2017-10-06 09:35:07 +0500

    curd

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 
-	validates :title, presence: true, length: {minimum: 5, maximum: 15}
-	validates :description, presence: true, length: {minimum: 12, maximum:110} 
+	validates :title, presence: true, length: {minimum: 3, maximum: 15}
+	validates :description, presence: true, length: {minimum: 10, maximum:110} 
 
 end
