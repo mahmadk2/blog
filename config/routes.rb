d658204d391ac5915007eb4bec9fdbd07dab5eae
ahmad <mahmadk2@hotmail.com>
2017-11-01 00:09:38 +0500

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 	get 'about', to: 'welcome#about'
 
 	resources :articles
+
+	get 'signup', to: 'users#new'
+	resources :users, expect: [:new]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
